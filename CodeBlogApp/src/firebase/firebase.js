14f7c1c381f90241edcf876b336e45542571f0c5
suraj sakhare <sakharesuraj10@gmail.com>
2024-05-09 09:19:50 +0530

    update git ignore file

--- CodeBlogApp/src/firebase/firebase.js
+++ CodeBlogApp/src/firebase/firebase.js
@@ -1,16 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDE53rSrGFQQaUSZ26tQZXdjOa-kCYZaL4",
-//   authDomain: "codewithsuraj-3bb7d.firebaseapp.com",
-//   projectId: "codewithsuraj-3bb7d",
-//   storageBucket: "codewithsuraj-3bb7d.appspot.com",
-//   messagingSenderId: "532052474228",
-//   appId: "1:532052474228:web:c972c6f5602b1afa9ed421",
-//   measurementId: "G-LJ0HS0QFQ5",
-//   databaseURL: "https://codewithsuraj-3bb7d-default-rtdb.firebaseio.com",
-// };
 const firebaseConfig = {
   apiKey:String(import.meta.env.VITE_API_KEY),
   authDomain:String(import.meta.env.VITE_AUTH_DOMAIN),
@@ -27,19 +16,15 @@ const analytics = getAnalytics(app);
 
 firebase.initializeApp(firebaseConfig);
 
-// Create a Firestore reference
 var db = firebase.firestore();
 
-// Fetch data from Firestore
 db.collection("blog")
   .get()
   .then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-      // doc.data() is an object representing the document
       console.log(doc.data());
     });
   })
   .catch((error) => {
     console.error("Error fetching data: ", error);
   });
-console.log(import.meta.env.VITE_API_KEY)
